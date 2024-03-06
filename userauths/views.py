2c0060b068c2f90b715db9c38ba4869b89ceb360
kaelkkd <kaelsiebra233@gmail.com>
2024-03-05 21:39:27 -0300

    simplified the register and login views

--- userauths/views.py
+++ userauths/views.py
@@ -12,13 +12,12 @@ def registerView(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST or None)
         if form.is_valid():
-            newUser = form.save()
-            username = form.cleaned_data.get("username")
+            form.save()
             messages.success(request, f"You account was succesfully created.")
-            newUser = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
-            login(request, newUser)
+            # newUser = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
+            # login(request, newUser)
 
-            return redirect("manager:index") #maybe redirect to the login page instead
+            return redirect("userauths:sign-in") #maybe redirect to the login page instead
     else:
         form = UserRegisterForm()
     
@@ -32,15 +31,15 @@ def loginView(request):
     if request.method == 'POST':
         form = UserAuthenticationForm(request.POST)
         if form.is_valid():
-            credentials = form.cleaned_data
-            user = authenticate(username=credentials['username'], password=credentials['password'])
+            # credentials = form.cleaned_data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
             if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return redirect('manager:dashboard')
+                login(request, user)
+                return redirect('manager/dashboard.html')
             else:
                 messages.error(request, f"The user does not exist.")
-
         else:
             messages.error(request, f'Invalid email/password.')
     else:
@@ -59,9 +58,6 @@ def logoutView(request):
 
 # @login_required(login_url='sign-in')
 # def dashboardView(request):
-
-
-
 #     context = {
 #         'temp':
 #     }
