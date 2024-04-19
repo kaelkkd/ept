90f6d08b1a5ecd26564743527f74d82c249b5938
kaelsiebra <kaelsiebra233@gmail.com>
2024-04-18 21:27:00 -0300

    wallet page added

--- manager/urls.py
+++ manager/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dashboard/transactions", views.transactions, name="transactions"),
+    path("dashboard/wallet", views.walletView, name="wallet"),
 ]
 
