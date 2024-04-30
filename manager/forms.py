00612831346817093f62aed8199a19d6aa3a94cb
kaelsiebra <kaelsiebra233@gmail.com>
2024-04-30 20:57:13 -0300

    added the wallet forms

--- manager/forms.py
+++ manager/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from userauths.models import User
 from .models import Wallet
 
 class WalletCreationForm(forms.Form):
     currency = forms.ChoiceField(choices=Wallet.CURRENCY)
     income = forms.DecimalField(max_digits=7, decimal_places=2)
     balance = forms.DecimalField(max_digits=10, decimal_places=2)
+
+class WalletUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Wallet
+        fields = ['balance', 'monthlyIncome', 'currencyType']
     
+# class TransactionCreationForm(forms.Form):
+#     value = forms.DecimalField(max_digits=10, decimal_places=2)
+#     # date = forms.DateField(auto_now=True)
+#     description = forms.TextInput()
