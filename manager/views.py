ce3c190e6b520d5024c7c22c1143daa4d8b3f145
kaelsiebra <kaelsiebra233@gmail.com>
2024-05-03 21:09:09 -0300

    fixed messages missing arguments and added the transaction view

--- manager/views.py
+++ manager/views.py
@@ -43,9 +43,9 @@ def updateWallet(request):
         form = WalletUpdateForm(request.POST, instance = wallet)
         if form.is_valid():
             form.save()
-            messages.success(f"The wallet was sucessfully updated.")
+            messages.success(request, f"The wallet was sucessfully updated.")
         else:
-            messages.error(f"There was an error, try again.")
+            messages.error(request, f"There was an error, try again.")
     else:
         form = WalletUpdateForm(instance=wallet)
 
@@ -58,7 +58,29 @@ def deleteWallet(request):
     wallet = get_object_or_404(Wallet, owner=request.user)
     if request.method == "POST":
         wallet.delete()
-        messages.success(f"Your wallet was sucessfully deleted.")
+        messages.success(request, f"Your wallet was sucessfully deleted.")
         return redirect('manager:wallet')
 
     return render(request, 'dashboard/delete-wallet.html')
+
+@login_required(login_url="sign-in/")
+def addTransaction(request):
+    if request.method == "POST":
+        form = TransactionCreationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = request.user
+            usrWallet, created = Wallet.objects.get_or_create(owner= user)
+            if created:
+                messages.warning(request, f"A new wallet was created.")
+
+            transaction = Transaction(wallet=usrWallet, value=data['value'], date=data['date'], description=data['description'])
+            transaction.save()
+            messages.success(request, f"Transaction sucessfully added.")
+            redirect('dashboard/transaction.html')
+    else:
+        form = TransactionCreationForm()
+
+    context = {'form':form}
+
+    return render(request, 'dashboard/add-transaction.html', context)
